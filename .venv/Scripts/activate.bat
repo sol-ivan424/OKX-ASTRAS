37d9b1595d0262d12c9ebe28d5a2be68dcf77ab1
sol-ivan424 <sol.ivan424@yandex.ru>
2025-11-12 19:00:03 +0300

    .venv connect

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\1\iCloudDrive\Desktop\Asrtras ALOR\.venv
+set VIRTUAL_ENV=C:\Users\1\Desktop\Astras-ALOR\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
